3e0b9f851dd2467c3db6336314a548b19f41ce60
nachocarher <nachoch64@correo.ugr.es>
2025-04-30 18:04:16 +0200

    Refactor App component to streamline state management and improve error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { useEffect, useReducer } from "react";
 
+const initialState = {
+  status: "loading", // loading, success, error
+  error: "",
+  data: [],
+  filters: {
+    name: "",
+    condition: "", // dead, alive, unknown
+  },
+  currentPage: 1,
+};
+
 function reducer(state, action) {
   switch (action.type) {
     case "setPage":
       return { ...state, currentPage: action.payload };
-    case "setData":
-      if (action.payload.error) {
-        console.log("error tio");
-        return state;
-      }
+    case "dataReceived":
       return { ...state, data: action.payload.results, status: "success" };
+    case "dataFailed":
+      return { ...state, status: "error", error: action.payload };
     default:
       throw new Error("Unknown");
   }
 }
 
 function App() {
-  const initialState = {
-    status: "loading", // loading, success, error
-    error: "",
-    data: [],
-    filters: {
-      name: "",
-      condition: "", // dead, alive, etc.
-    },
-    currentPage: 1,
-  };
-
-  const [{ status, data, currentPage }, dispatch] = useReducer(
+  const [{ status, data, currentPage, error }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -35,20 +33,26 @@ function App() {
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`)
       .then((response) => response.json())
-      .then((response) => dispatch({ type: "setData", payload: response }))
-      .catch((err) => console.error(err));
+      .then((response) => dispatch({ type: "dataReceived", payload: response }))
+      .catch((err) => dispatch({ type: "dataFailed", payload: err }));
   }, [currentPage]);
 
   return (
-    <div>
+    <main>
+      {status === "loading" && <p>Loading...</p>}
+      {status === "error" && (
+        <p>
+          <span>💥</span> {error}
+        </p>
+      )}
       {status === "success" &&
-        data.map((character) => (
+        data?.map((character) => (
           <div className="character" key={character.id}>
             <h2>{character.name}</h2>
             <img src={character.image} alt={character.name} />
           </div>
         ))}
-    </div>
+    </main>
   );
 }
 
