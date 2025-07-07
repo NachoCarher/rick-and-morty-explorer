f34c82e23a6bb02cfe6ef52e8fa42cd5c68b422c
nachocarher <nachoch64@correo.ugr.es>
2025-07-07 18:10:40 +0200

    Improve styling of favorites link and button

--- src/App.jsx
+++ src/App.jsx
@@ -7,50 +7,51 @@ import { Link } from "react-router";
 import { useCharacter } from "./contexts/CharacterContext";
 
 function App() {
-    const { dispatch, favCharacters, error, characters, status } =
-        useCharacter();
-    const page = useRef(1);
+  const { dispatch, favCharacters, error, characters, status } = useCharacter();
+  const page = useRef(1);
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        page.current = 1;
+  function handleSubmit(event) {
+    event.preventDefault();
+    page.current = 1;
 
-        fetch(
-            `https://rickandmortyapi.com/api/character/?name=${event.target[0].value}`
-        )
-            .then((response) => response.json())
-            .then((data) => dispatch({ type: "dataReceived", payload: data }))
-            .catch((err) => dispatch({ type: "dataFailed", payload: err }));
-    }
+    fetch(
+      `https://rickandmortyapi.com/api/character/?name=${event.target[0].value}`
+    )
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed", payload: err }));
+  }
 
-    return (
-        <main>
-            <ul>
-                {favCharacters
-                    ? favCharacters.map((favCharacter) => (
-                          <li key={favCharacter.id}>{favCharacter.name}</li>
-                      ))
-                    : "There are no favs"}{" "}
-            </ul>
-            <h1>Rick and Morty explorer</h1>
+  return (
+    <main>
+      <ul>
+        {favCharacters
+          ? favCharacters.map((favCharacter) => (
+              <li key={favCharacter.id}>{favCharacter.name}</li>
+            ))
+          : "There are no favs"}{" "}
+      </ul>
+      <h1>Rick and Morty explorer</h1>
 
-            <nav>
-                <Link to="/favorites">Favorites</Link>
-            </nav>
+      <nav>
+        <Link to="/favorites" className="fav-link">
+          Favorites
+        </Link>
+      </nav>
 
-            <Search onSubmit={handleSubmit} />
+      <Search onSubmit={handleSubmit} />
 
-            {status === "loading" && <p>Loading...</p>}
-            {status === "error" && (
-                <p>
-                    <span>💥</span> {error}
-                </p>
-            )}
-            {status === "success" && <CharactersList characters={characters} />}
+      {status === "loading" && <p>Loading...</p>}
+      {status === "error" && (
+        <p>
+          <span>💥</span> {error}
+        </p>
+      )}
+      {status === "success" && <CharactersList characters={characters} />}
 
-            <PageNavigation page={page} />
-        </main>
-    );
+      <PageNavigation page={page} />
+    </main>
+  );
 }
 
 export default App;
--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,30 +1,32 @@
 import { useCharacter } from "../contexts/CharacterContext";
 import Character from "./Character";
 
+import icon from "../assets/fav-icon.png";
+
 /* eslint-disable react/prop-types */
 function CharactersList({ characters }) {
-    const { dispatch } = useCharacter();
+  const { dispatch } = useCharacter();
 
-    return (
-        <ul className="character-container">
-            {characters?.map((character) => (
-                <li className="character" key={character.id}>
-                    <Character name={character.name} image={character.image} />
-                    <button
-                        className="fav-button"
-                        onClick={() =>
-                            dispatch({
-                                type: "addFavCharacter",
-                                payload: character,
-                            })
-                        }
-                    >
-                        ⭐
-                    </button>
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className="character-container">
+      {characters?.map((character) => (
+        <li className="character" key={character.id}>
+          <button
+            className="fav-button"
+            onClick={() =>
+              dispatch({
+                type: "addFavCharacter",
+                payload: character,
+              })
+            }
+          >
+            <img src={icon} alt="favorite-icon" />
+          </button>
+          <Character name={character.name} image={character.image} />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default CharactersList;
